2378d01b579b6d97742f0893bae8e6fe057b580d
Aiden <aiden.marques94@gmail.com>
2021-04-09 15:49:10 -0400

    commit tree 9f322fd8cb9dbe5caf6045f045214bd207b4ef43

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  
   # GET /posts
   # GET /posts.json
   def index
@@ -68,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [])
+      params.require(:post).permit(:name, :content, :tag_ids => [], :tags_attributes => [:name])
     end
 end
